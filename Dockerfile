832e087e8766b3716e8a36d79bae749c97b2695b
rafaela <rafaela.augusto@somei.com.vc>
2022-10-16 18:00:36 -0300

    Finalized simple api flask

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-# Derivando da imagem oficial do postgres
-FROM postgres
-
-# Adicionando os scripts SQL para serem executados na criação do banco
-COPY ./db/ /docker-entrypoint-initdb.d/
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from re import M
+from statistics import mode
 from flask import Flask, make_response, jsonify, request
 from bd import carros
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/')
+def root():
+    return "<h1>API de Carros</h1>" 
 
 @app.route('/carros', methods=['GET'])
 def get_carros():
@@ -19,4 +25,31 @@ def create_carro():
         carro=carro     
     ))
 
-app.run()
+@app.route('/carros/<int:carro_id>', methods=['GET'])
+def get_carro(carro_id: int):
+    carro = carros[carro_id-1]
+    if carro:
+        return make_response(jsonify(carro=carro))
+
+@app.route('/carros/<int:carro_id>', methods=['DELETE'])
+def delete_carros(carro_id: int):
+    carro = carros[carro_id-1]
+    if carro:
+        carros.remove(carro)
+    return get_carros()
+
+@app.route('/carros/<int:carro_id>', methods=['PUT'])
+def update(carro_id: int):
+    body = request.json
+    marca = body['marca']
+    modelo = body['modelo']
+    ano = body['ano']
+
+    carros[carro_id-1]['marca'] = marca
+    carros[carro_id-1]['modelo'] = modelo
+    carros[carro_id-1]['ano'] = ano
+    
+    return make_response(jsonify(
+        carros=carros[carro_id-1]))
+        
+app.run(debug=True)
